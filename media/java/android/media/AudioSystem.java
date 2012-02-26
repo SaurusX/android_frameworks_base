415dd2238aaca221e098ea4daa9674b24e5126c8
SaurusX <jake.hurtado@gmail.com>
2012-02-26 11:52:05 -0600

    Added Motorola stock setting of DEVICE_OUT_HDMI = 0x1000 and changed DEVICE_OUT_LOW_POWER to 0x900 to avoid conflict.

--- media/java/android/media/AudioSystem.java
+++ media/java/android/media/AudioSystem.java
@@ -247,6 +247,7 @@ public class AudioSystem
     public static final int DEVICE_OUT_BLUETOOTH_A2DP_SPEAKER = 0x200;
     public static final int DEVICE_OUT_AUX_DIGITAL = 0x400;
     public static final int DEVICE_OUT_FM = 0x800;
+    public static final int DEVICE_OUT_HDMI = 0x1000;
     public static final int DEVICE_OUT_DEFAULT = 0x8000;
     // input devices
     public static final int DEVICE_IN_COMMUNICATION = 0x10000;
@@ -263,7 +264,7 @@ public class AudioSystem
     // For OMAP
     public static final int DEVICE_OUT_FM_TRANSMIT = 0x800;
     public static final int DEVICE_IN_FM_ANALOG = 0x1000000;
-    public static final int DEVICE_OUT_LOW_POWER = 0x1000;
+    public static final int DEVICE_OUT_LOW_POWER = 0x900;
 
     // device states
     public static final int DEVICE_STATE_UNAVAILABLE = 0;
